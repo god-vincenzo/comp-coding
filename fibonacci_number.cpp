6b0d1ae31b62bb3dd745a43bf124f8b161caaa05
Sameer Thakur <st2410thakur@gmail.com>
2025-08-04 00:27:43 +0530

    Add solution for Fibonacci Number using iterative approach
    Calculated the nth Fibonacci number using bottom-up iteration (tabulation).
    Avoided recursion to improve performance and avoid stack overflow for large n.
    Time Complexity: O(n), Space Complexity: O(1)

---
+++ fibonacci_number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int fib1 = 0;
+        int fib2 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int curr = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = curr;
+        }
+        return fib2;
+    }
+};
